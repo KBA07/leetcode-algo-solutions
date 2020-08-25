c0da4643fa9f50bfa458cb3ad7091e94c12fc35b
Kashif Ali <kashif.bilal.ali07@gmail.com>
2020-08-25 19:41:16 +0530

    997_find_town_judge

---
+++ algo/997_find_town_judge.py
@@ -0,0 +1,70 @@
+"""
+In a town, there are N people labelled from 1 to N.  There is a rumor that one of these people is secretly the town judge.
+
+If the town judge exists, then:
+
+The town judge trusts nobody.
+Everybody (except for the town judge) trusts the town judge.
+There is exactly one person that satisfies properties 1 and 2.
+You are given trust, an array of pairs trust[i] = [a, b] representing that the person labelled a trusts the person labelled b.
+
+If the town judge exists and can be identified, return the label of the town judge.  Otherwise, return -1.
+
+
+
+Example 1:
+
+Input: N = 2, trust = [[1,2]]
+Output: 2
+Example 2:
+
+Input: N = 3, trust = [[1,3],[2,3]]
+Output: 3
+Example 3:
+
+Input: N = 3, trust = [[1,3],[2,3],[3,1]]
+Output: -1
+Example 4:
+
+Input: N = 3, trust = [[1,2],[2,3]]
+Output: -1
+Example 5:
+
+Input: N = 4, trust = [[1,3],[1,4],[2,3],[2,4],[4,3]]
+Output: 3
+
+
+Constraints:
+
+1 <= N <= 1000
+0 <= trust.length <= 10^4
+trust[i].length == 2
+trust[i] are all different
+trust[i][0] != trust[i][1]
+1 <= trust[i][0], trust[i][1] <= N
+"""
+"""
+Runtime: 968 ms, faster than 26.18% of Python online submissions for Find the Town Judge.
+Memory Usage: 17.8 MB, less than 69.29% of Python online submissions for Find the Town Judge.
+"""
+
+class Solution(object):
+    def findJudge(self, N, trust):
+        """
+        :type N: int
+        :type trust: List[List[int]]
+        :rtype: int
+        """
+        if N == 1:
+            return 1
+
+        trustCount = [0] * (N + 1)
+
+        for person1 , person2 in trust:
+            trustCount[person1] -= 1
+            trustCount[person2] += 1
+
+        if N-1 in trustCount:
+            return trustCount.index(N-1)
+
+        return -1
