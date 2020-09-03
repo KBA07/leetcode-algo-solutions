c1793920e31755fd0b318cab8e351eb78880ed13
Kashif Ali <kashif.bilal.ali07@gmail.com>
2020-09-03 22:54:21 +0530

    Minimum in rotated sorted array

---
+++ algo/153_find_minimum_in_rotated_sored_array.py
@@ -0,0 +1,39 @@
+"""
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+
+Find the minimum element.
+
+You may assume no duplicate exists in the array.
+
+Example 1:
+
+Input: [3,4,5,1,2]
+Output: 1
+Example 2:
+
+Input: [4,5,6,7,0,1,2]
+Output: 0
+"""
+"""
+Runtime: 24 ms, faster than 92.92% of Python online submissions for Find Minimum in Rotated Sorted Array.
+Memory Usage: 12.9 MB, less than 75.56% of Python online submissions for Find Minimum in Rotated Sorted Array.
+"""
+
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        low, high = 0 , len(nums) - 1
+
+        while low < high:
+            mid = low + (high-low)//2
+            if nums[mid] > nums[high]:
+                low = mid + 1
+            else:
+                high = mid
+
+        return nums[low]
