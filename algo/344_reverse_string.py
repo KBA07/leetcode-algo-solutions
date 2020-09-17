a9ba1fa27632d1016e3cb2f65156f45bf08b7501
Kashif Ali <kashif.bilal.ali07@gmail.com>
2020-09-17 17:01:46 +0530

    reverse_string

---
+++ algo/344_reverse_string.py
@@ -0,0 +1,33 @@
+"""
+Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+
+
+
+Example 1:
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+"""
+
+"""
+Runtime: 172 ms, faster than 85.33% of Python online submissions for Reverse String.
+Memory Usage: 19.7 MB, less than 98.87% of Python online submissions for Reverse String.
+"""
+
+class Solution(object):
+    def reverseString(self, s):
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
