71956db29370ed52bdeaaae4b24b9c2d6413e552
KBA07 <kashif.bilal.ali07@gmail.com>
2021-06-14 21:45:23 +0530

    121_best_time_to_buy_and_sell_stock.py

---
+++ arrays/121_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,51 @@
+"""
+Share
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+Accepted
+1,364,622
+Submissions
+2,618,206
+"""
+"""
+Success
+Details 
+Runtime: 808 ms, faster than 91.10% of Python online submissions for Best Time to Buy and Sell Stock.
+Memory Usage: 22.8 MB, less than 32.72% of Python online submissions for Best Time to Buy and Sell Stock.
+"""
+
+class Solution(object):
+    def maxProfit(self, prices):
+        minimum = prices[0]
+        max_profit = 0
+        for index, price in enumerate(prices):
+            if minimum > price:
+                minimum = price
+                
+            if max_profit < (price - minimum):
+                max_profit = price - minimum
+        
+        return max_profit
