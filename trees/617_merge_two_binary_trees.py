21efeae41c9a3ba1bc86af850bab7f9abbe1679c
KBA07 <kashif.bilal.ali07@gmail.com>
2021-06-15 20:54:19 +0530

    Iterative trees/617_merge_two_binary_trees.py

--- trees/617_merge_two_binary_trees.py
+++ trees/617_merge_two_binary_trees.py
@@ -59,3 +59,43 @@ class Solution(object):
         root1.left = self.mergeTrees(root1.left, root2.left)
         root1.right = self.mergeTrees(root1.right, root2.right)
         return root1
+
+"""
+Iterative approach
+Runtime: 76 ms, faster than 76.03% of Python online submissions for Merge Two Binary Trees.
+Memory Usage: 14.6 MB, less than 41.40% of Python online submissions for Merge Two Binary Trees.
+"""
+
+class Solution(object):
+    def mergeTrees(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: TreeNode
+        """
+        if not root1:
+            return root2
+        
+        stack = []
+        stack.append((root1, root2))
+        
+        while len(stack) != 0:
+            node1, node2 = stack.pop()
+            
+            if not node1 or not node2:
+                continue
+                
+            node1.val += node2.val
+                
+            if not node1.left:
+                node1.left = node2.left
+            else:
+                stack.append((node1.left, node2.left))
+                
+            
+            if not node1.right:
+                node1.right = node2.right
+            else:
+                stack.append((node1.right, node2.right))
+        
+        return root1
