486b13f4c7b62df9864f4d4538587399b121f5e2
KBA07 <kashif.bilal.ali07@gmail.com>
2021-06-19 20:03:59 +0530

    Adding dp and fibonachi approach

--- recursion/70_climbing_stairs.py
+++ recursion/70_climbing_stairs.py
@@ -59,3 +59,36 @@ class Solution:
         
         self.cache[n] = self.climbStairs(n-2) + self.climbStairs(n-1)
         return self.cache[n] 
+
+# DP
+"""
+Runtime: 40 ms, faster than 13.99% of Python3 online submissions for Climbing Stairs.
+Memory Usage: 14.2 MB, less than 71.59% of Python3 online submissions for Climbing Stairs.
+"""
+class Solution:
+    def climbStairs(self, n: int) -> int: 
+        dp = [0, 1, 2]
+        if (n==1): return dp[1]
+        
+        for index in range(3, n+1):
+            dp.append(dp[index-1] + dp[index-2])
+        
+        return dp[n]
+
+# Fibonachi as we have total number of steps fn = f(n-1) + f(n-2)
+"""
+Runtime: 32 ms, faster than 54.51% of Python3 online submissions for Climbing Stairs.
+Memory Usage: 14.3 MB, less than 41.99% of Python3 online submissions for Climbing Stairs.
+"""
+class Solution:
+    def climbStairs(self, n: int) -> int:  
+        first = 1
+        if (n==1): return first
+        second = 2
+        
+        for _ in range(3, n+1):
+            third = first + second
+            first = second
+            second = third
+        
+        return second
