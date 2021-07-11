06a37a3c1497f8d3b2864528a821e882a825d35d
KBA07 <kashif.bilal.ali07@gmail.com>
2021-07-11 17:39:38 +0530

    trees/104_maximum_depth_of_binary_tree.py

--- trees/104_maximum_depth_of_binary_tree.py
+++ trees/104_maximum_depth_of_binary_tree.py
@@ -44,6 +44,24 @@ Submissions
 1,736,140
 """
 
+# DFS
+"""
+Runtime: 32 ms, faster than 98.15% of Python3 online submissions for Maximum Depth of Binary Tree.
+Memory Usage: 16.2 MB, less than 38.98% of Python3 online submissions for Maximum Depth of Binary Tree.
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
 # BFS
 """
 Runtime: 64 ms, faster than 7.64% of Python3 online submissions for Maximum Depth of Binary Tree.
