8eab28958a04532974a611c7e09dccadf5876bf6
KBA07 <kashif.bilal.ali07@gmail.com>
2021-07-19 22:46:34 +0530

    trees/235_lowest_common_ancestor_of_a_binary_search_tree.py

---
+++ trees/235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -0,0 +1,69 @@
+"""
+235. Lowest Common Ancestor of a Binary Search Tree
+Easy
+
+3607
+
+146
+
+Add to List
+
+Share
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+ 
+
+Example 1:
+
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+Example 2:
+
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
+Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+Example 3:
+
+Input: root = [2,1], p = 2, q = 1
+Output: 2
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 105].
+-109 <= Node.val <= 109
+All Node.val are unique.
+p != q
+p and q will exist in the BST.
+Accepted
+547,246
+Submissions
+1,022,259
+"""
+
+"""
+Runtime: 76 ms, faster than 76.35% of Python3 online submissions for Lowest Common Ancestor of a Binary Search Tree.
+Memory Usage: 18.2 MB, less than 47.13% of Python3 online submissions for Lowest Common Ancestor of a Binary Search Tree.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+        
