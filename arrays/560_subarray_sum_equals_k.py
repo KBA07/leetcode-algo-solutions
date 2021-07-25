d3797f49982e0b2755274dcd91aef07947758df1
KBA07 <kashif.bilal.ali07@gmail.com>
2021-07-25 11:45:04 +0530

    arrays/560_subarray_sum_equals_k.py

--- arrays/560_subarray_sum_equals_k.py
+++ arrays/560_subarray_sum_equals_k.py
@@ -68,3 +68,48 @@ class Solution:
                     count += 1
         
         return count
+
+"""
+Time Limit Exceeded
+72 / 89 test cases passed.
+"""
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        # sum_map = [0] * (len(nums) + 1)
+        
+        # for index in range(1, len(nums) + 1):
+        #     sum_map[index] = sum_map[index - 1] + nums[index - 1]
+
+        count = 0
+        for i in range(len(nums)):
+            cur_sum = 0
+            for j in range(i, len(nums)):
+                cur_sum += nums[j]
+                
+                if cur_sum == k:
+                    count += 1
+        
+        return count
+
+"""
+Runtime: 272 ms, faster than 35.27% of Python3 online submissions for Subarray Sum Equals K.
+Memory Usage: 16.8 MB, less than 25.88% of Python3 online submissions for Subarray Sum Equals K.
+"""
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        sum_map = {0: 1}
+        
+        curr_sum = 0
+        count = 0
+        for index in range(len(nums)):
+            curr_sum += nums[index]
+            
+            if (curr_sum - k) in sum_map:
+                count += sum_map[curr_sum - k]
+            
+            try:
+                sum_map[curr_sum] += 1
+            except KeyError:
+                sum_map[curr_sum] = 1
+        
+        return count
