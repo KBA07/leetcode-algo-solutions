3642d17c5965e67ab37f23a1f9ccb0f558379f9f
KBA07 <kashif.bilal.ali07@gmail.com>
2021-07-25 13:13:49 +0530

    algo/946_validate_stack_sequences.py

--- algo/946_validate_stack_sequences.py
+++ algo/946_validate_stack_sequences.py
@@ -81,3 +81,20 @@ class Solution:
                 j += 1
         
         return not stack
+
+"""
+Runtime: 68 ms, faster than 84.78% of Python3 online submissions for Validate Stack Sequences.
+Memory Usage: 14.6 MB, less than 27.99% of Python3 online submissions for Validate Stack Sequences.
+"""
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        
+        i = j = 0
+        for num in pushed:
+            pushed[i] = num
+            while i >= 0 and pushed[i] == popped[j]:
+                i -= 1
+                j += 1
+            i += 1
+        
+        return i == 0
