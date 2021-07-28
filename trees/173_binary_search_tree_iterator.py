070474dc78fc291c601a1bf35867dcb21c50f627
KBA07 <kashif.bilal.ali07@gmail.com>
2021-07-28 23:27:26 +0530

    trees/173_binary_search_tree_iterator.py

--- trees/173_binary_search_tree_iterator.py
+++ trees/173_binary_search_tree_iterator.py
@@ -102,3 +102,43 @@ class BSTIterator:
 # obj = BSTIterator(root)
 # param_1 = obj.next()
 # param_2 = obj.hasNext()
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+"""
+Runtime: 102 ms, faster than 13.40% of Python3 online submissions for Binary Search Tree Iterator.
+Memory Usage: 20.6 MB, less than 33.82% of Python3 online submissions for Binary Search Tree Iterator.
+"""
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.stack = []
+        self.currentNode = root
+        
+        
+    def next(self) -> int:
+        while self.currentNode:
+            self.stack.append(self.currentNode)
+            self.currentNode = self.currentNode.left
+        
+        last = self.stack.pop()
+        if last.right:
+            self.currentNode = last.right
+        
+        return last.val
+        
+
+    def hasNext(self) -> bool:
+        return self.currentNode is not None or len(self.stack) > 0
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
