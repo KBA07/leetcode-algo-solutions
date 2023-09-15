8160f77fe49ca4180a500f84e3f07bd23708b897
KBA07 <kashif.bilal.ali07@gmail.com>
2023-09-16 01:16:06 +0530

    maths/2413_smallest_even_multiple.py

---
+++ maths/2413_smallest_even_multiple.py
@@ -0,0 +1,43 @@
+"""
+2413. Smallest Even Multiple
+Easy
+741
+81
+Companies
+Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+ 
+
+Example 1:
+
+Input: n = 5
+Output: 10
+Explanation: The smallest multiple of both 5 and 2 is 10.
+Example 2:
+
+Input: n = 6
+Output: 6
+Explanation: The smallest multiple of both 6 and 2 is 6. Note that a number is a multiple of itself.
+ 
+
+Constraints:
+
+1 <= n <= 150
+Accepted
+111.9K
+Submissions
+128.1K
+Acceptance Rate
+87.3%
+"""
+
+"""
+Runtime: 36ms Beats 73.75% of users with Python3
+Memory Usage: 16.25MB Beats 54.07% of users with Python3
+"""
+
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n % 2 == 0: return n
+
+        return n * 2
+        
